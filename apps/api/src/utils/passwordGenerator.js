3c225a54593f4d854b31543127aa47eb9054ba1a
Márcio Corrêa <marcio.ramos.correa@gmail.com>
2025-08-18 22:14:39 -0300

    Docker e remocao de arquivos

--- apps/api/src/utils/passwordGenerator.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-function generateSecurityPassword(length = 10) {
-  return crypto.randomBytes(length).toString('base64').slice(0, length);
-}
-
-module.exports = { generateSecurityPassword };
